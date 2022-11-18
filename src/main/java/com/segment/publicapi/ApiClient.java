792abaeecc4660ad91b5ccce9013a0da491311ee
public-api ci <team-apics@twilio.com>
2022-11-18 16:21:18 +0000

    Automated client deploy (from segmentio/public-api@1a1e767)

--- src/main/java/com/segment/publicapi/ApiClient.java
+++ src/main/java/com/segment/publicapi/ApiClient.java
@@ -124,7 +124,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/33.0.0/java");
+        setUserAgent("OpenAPI-Generator/33.0.1/java");
 
         authentications = new HashMap<String, Authentication>();
     }
