7875cd54c7cf5ab2cb5be48240b8e20f22fd2270
APIs and Common Services team <team-apics@twilio.com>
2023-06-28 19:18:05 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@2a68676.

--- src/main/java/com/segment/publicapi/api/DestinationsApi.java
+++ src/main/java/com/segment/publicapi/api/DestinationsApi.java
@@ -1147,8 +1147,8 @@ public class DestinationsApi {
      * List Delivery Metrics Summary from Destination Get an event delivery metrics summary from a
      * Destination. Based on the granularity chosen, there are restrictions on the time range you
      * can query: **Minute**: - Max time range: 4 hours - Oldest possible start time: 48 hours in
-     * the past **Hour**: - Max Time range: 1 week - Oldest possible start time: 10 days in the past
-     * **Day**: - Max time range: 60 days - Oldest possible start time: 60 days in the past The rate
+     * the past **Hour**: - Max Time range: 7 days - Oldest possible start time: 7 days in the past
+     * **Day**: - Max time range: 14 days - Oldest possible start time: 14 days in the past The rate
      * limit for this endpoint is 60 requests per minute, which is lower than the default due to
      * access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP
      * status code with headers indicating the limit parameters. See [Rate
@@ -1195,8 +1195,8 @@ public class DestinationsApi {
      * List Delivery Metrics Summary from Destination Get an event delivery metrics summary from a
      * Destination. Based on the granularity chosen, there are restrictions on the time range you
      * can query: **Minute**: - Max time range: 4 hours - Oldest possible start time: 48 hours in
-     * the past **Hour**: - Max Time range: 1 week - Oldest possible start time: 10 days in the past
-     * **Day**: - Max time range: 60 days - Oldest possible start time: 60 days in the past The rate
+     * the past **Hour**: - Max Time range: 7 days - Oldest possible start time: 7 days in the past
+     * **Day**: - Max time range: 14 days - Oldest possible start time: 14 days in the past The rate
      * limit for this endpoint is 60 requests per minute, which is lower than the default due to
      * access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP
      * status code with headers indicating the limit parameters. See [Rate
@@ -1245,8 +1245,8 @@ public class DestinationsApi {
      * List Delivery Metrics Summary from Destination (asynchronously) Get an event delivery metrics
      * summary from a Destination. Based on the granularity chosen, there are restrictions on the
      * time range you can query: **Minute**: - Max time range: 4 hours - Oldest possible start time:
-     * 48 hours in the past **Hour**: - Max Time range: 1 week - Oldest possible start time: 10 days
-     * in the past **Day**: - Max time range: 60 days - Oldest possible start time: 60 days in the
+     * 48 hours in the past **Hour**: - Max Time range: 7 days - Oldest possible start time: 7 days
+     * in the past **Day**: - Max time range: 14 days - Oldest possible start time: 14 days in the
      * past The rate limit for this endpoint is 60 requests per minute, which is lower than the
      * default due to access pattern restrictions. Once reached, this endpoint will respond with the
      * 429 HTTP status code with headers indicating the limit parameters. See [Rate
