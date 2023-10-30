fac50aad918b2de8b6ad25ca2b6d0ed54a04d80f
APIs and Common Services team <team-apics@twilio.com>
2023-10-30 23:53:00 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@8be4a5ea.

--- src/main/java/com/segment/publicapi/ApiClient.java
+++ src/main/java/com/segment/publicapi/ApiClient.java
@@ -123,7 +123,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Public API SDK 36.3.1 (Java)");
+        setUserAgent("Public API SDK 37.0.0 (Java)");
 
         authentications = new HashMap<String, Authentication>();
     }
