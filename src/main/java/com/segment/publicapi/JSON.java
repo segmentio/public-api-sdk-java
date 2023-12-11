d9d99750ca747ac8d587950f4cf789f5b8333772
APIs and Common Services team <team-apics@twilio.com>
2023-12-11 17:36:28 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@2e20e535.

--- src/main/java/com/segment/publicapi/JSON.java
+++ src/main/java/com/segment/publicapi/JSON.java
@@ -274,6 +274,15 @@ public class JSON {
                 new com.segment.publicapi.models.CreateProfilesWarehouseAlphaOutput
                         .CustomTypeAdapterFactory());
         gsonBuilder.registerTypeAdapterFactory(
+                new com.segment.publicapi.models.CreateReverseETLManualSync200Response
+                        .CustomTypeAdapterFactory());
+        gsonBuilder.registerTypeAdapterFactory(
+                new com.segment.publicapi.models.CreateReverseETLManualSyncInput
+                        .CustomTypeAdapterFactory());
+        gsonBuilder.registerTypeAdapterFactory(
+                new com.segment.publicapi.models.CreateReverseETLManualSyncOutput
+                        .CustomTypeAdapterFactory());
+        gsonBuilder.registerTypeAdapterFactory(
                 new com.segment.publicapi.models.CreateReverseEtlModel200Response
                         .CustomTypeAdapterFactory());
         gsonBuilder.registerTypeAdapterFactory(
@@ -1194,6 +1203,9 @@ public class JSON {
                         .CustomTypeAdapterFactory());
         gsonBuilder.registerTypeAdapterFactory(
                 new com.segment.publicapi.models.RestoreFunctionVersionAlphaOutput
+                        .CustomTypeAdapterFactory());
+        gsonBuilder.registerTypeAdapterFactory(
+                new com.segment.publicapi.models.ReverseETLManualSyncJobOutput
                         .CustomTypeAdapterFactory());
         gsonBuilder.registerTypeAdapterFactory(
                 new com.segment.publicapi.models.ReverseEtlModel.CustomTypeAdapterFactory());
