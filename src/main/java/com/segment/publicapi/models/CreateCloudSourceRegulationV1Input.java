505cafcf19a18813d714ad4e5bb754737c2db76c
APIs and Common Services team <team-apics@twilio.com>
2024-04-18 18:30:48 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@988bcc51.

--- src/main/java/com/segment/publicapi/models/CreateCloudSourceRegulationV1Input.java
+++ src/main/java/com/segment/publicapi/models/CreateCloudSourceRegulationV1Input.java
@@ -43,6 +43,8 @@ public class CreateCloudSourceRegulationV1Input {
 
         SUPPRESS_WITH_DELETE("SUPPRESS_WITH_DELETE"),
 
+        SUPPRESS_WITH_DELETE_INTERNAL("SUPPRESS_WITH_DELETE_INTERNAL"),
+
         UNSUPPRESS("UNSUPPRESS");
 
         private String value;
