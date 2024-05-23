96c1e8a3396ddd9883fb29242885cc13721ea941
APIs and Common Services team <team-apics@twilio.com>
2024-05-23 12:17:43 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@8244c799.

--- src/main/java/com/segment/publicapi/models/Definition1.java
+++ src/main/java/com/segment/publicapi/models/Definition1.java
@@ -48,7 +48,9 @@ public class Definition1 {
     }
 
     /**
-     * The query language string defining the audience segmentation criteria.
+     * The query language string defining the computed trait aggregation criteria. For guidance on
+     * using the query language, see the [Segment documentation
+     * site](https://segment.com/docs/api/public-api/query-language).
      *
      * @return query
      */
@@ -68,7 +70,8 @@ public class Definition1 {
     }
 
     /**
-     * The underlying data type being segmented for this audience. Possible values: users, accounts.
+     * The underlying data type being aggregated for this computed trait. Possible values: users,
+     * accounts.
      *
      * @return type
      */
