643cb24d98c7b7c3afdfa20edc0bb57e92341ca9
APIs and Common Services team <team-apics@twilio.com>
2024-05-23 19:41:32 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@e86e46b9.

--- src/main/java/com/segment/publicapi/models/AudienceComputationDefinition.java
+++ src/main/java/com/segment/publicapi/models/AudienceComputationDefinition.java
@@ -16,7 +16,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
-import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
@@ -30,58 +29,10 @@ import java.util.Set;
 
 /** AudienceComputationDefinition */
 public class AudienceComputationDefinition {
-    /**
-     * The underlying data type being segmented for this audience. Possible values: users, accounts.
-     */
-    @JsonAdapter(TypeEnum.Adapter.class)
-    public enum TypeEnum {
-        ACCOUNTS("accounts"),
-
-        USERS("users");
-
-        private String value;
-
-        TypeEnum(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return String.valueOf(value);
-        }
-
-        public static TypeEnum fromValue(String value) {
-            for (TypeEnum b : TypeEnum.values()) {
-                if (b.value.equals(value)) {
-                    return b;
-                }
-            }
-            throw new IllegalArgumentException("Unexpected value '" + value + "'");
-        }
-
-        public static class Adapter extends TypeAdapter<TypeEnum> {
-            @Override
-            public void write(final JsonWriter jsonWriter, final TypeEnum enumeration)
-                    throws IOException {
-                jsonWriter.value(enumeration.getValue());
-            }
-
-            @Override
-            public TypeEnum read(final JsonReader jsonReader) throws IOException {
-                String value = jsonReader.nextString();
-                return TypeEnum.fromValue(value);
-            }
-        }
-    }
-
     public static final String SERIALIZED_NAME_TYPE = "type";
 
     @SerializedName(SERIALIZED_NAME_TYPE)
-    private TypeEnum type;
+    private String type;
 
     public static final String SERIALIZED_NAME_QUERY = "query";
 
@@ -90,7 +41,7 @@ public class AudienceComputationDefinition {
 
     public AudienceComputationDefinition() {}
 
-    public AudienceComputationDefinition type(TypeEnum type) {
+    public AudienceComputationDefinition type(String type) {
 
         this.type = type;
         return this;
@@ -102,11 +53,11 @@ public class AudienceComputationDefinition {
      * @return type
      */
     @javax.annotation.Nonnull
-    public TypeEnum getType() {
+    public String getType() {
         return type;
     }
 
-    public void setType(TypeEnum type) {
+    public void setType(String type) {
         this.type = type;
     }
 
