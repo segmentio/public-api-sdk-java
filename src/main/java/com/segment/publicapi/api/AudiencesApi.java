f1ae0d41434ab6c4a577d49203ef5bc8c5392e39
APIs and Common Services team <team-apics@twilio.com>
2025-10-06 19:35:12 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@bed028d4.

--- src/main/java/com/segment/publicapi/api/AudiencesApi.java
+++ src/main/java/com/segment/publicapi/api/AudiencesApi.java
@@ -201,10 +201,12 @@ public class AudiencesApi {
 
     /**
      * Add Audience Schedule to Audience The ability to configure the run schedule for an Audience
-     * is limited to Linked Audiences (audienceType &#x3D; LINKED). • This endpoint is in **Alpha**
-     * testing. Please submit any feedback by sending an email to friends@segment.com. • In order to
-     * successfully call this endpoint, the specified Workspace needs to have the Audience feature
-     * enabled. Please reach out to your customer success manager for more information.
+     * is limited to Linked Audiences (audienceType &#x3D; LINKED). Note that if a Linked Audience
+     * remains disabled for 90 days Segment will delete the associated schedule and a new schedule
+     * will need to be created. • This endpoint is in **Alpha** testing. Please submit any feedback
+     * by sending an email to friends@segment.com. • In order to successfully call this endpoint,
+     * the specified Workspace needs to have the Audience feature enabled. Please reach out to your
+     * customer success manager for more information.
      *
      * @param spaceId (required)
      * @param id (required)
@@ -234,10 +236,12 @@ public class AudiencesApi {
 
     /**
      * Add Audience Schedule to Audience The ability to configure the run schedule for an Audience
-     * is limited to Linked Audiences (audienceType &#x3D; LINKED). • This endpoint is in **Alpha**
-     * testing. Please submit any feedback by sending an email to friends@segment.com. • In order to
-     * successfully call this endpoint, the specified Workspace needs to have the Audience feature
-     * enabled. Please reach out to your customer success manager for more information.
+     * is limited to Linked Audiences (audienceType &#x3D; LINKED). Note that if a Linked Audience
+     * remains disabled for 90 days Segment will delete the associated schedule and a new schedule
+     * will need to be created. • This endpoint is in **Alpha** testing. Please submit any feedback
+     * by sending an email to friends@segment.com. • In order to successfully call this endpoint,
+     * the specified Workspace needs to have the Audience feature enabled. Please reach out to your
+     * customer success manager for more information.
      *
      * @param spaceId (required)
      * @param id (required)
@@ -270,11 +274,12 @@ public class AudiencesApi {
 
     /**
      * Add Audience Schedule to Audience (asynchronously) The ability to configure the run schedule
-     * for an Audience is limited to Linked Audiences (audienceType &#x3D; LINKED). • This endpoint
-     * is in **Alpha** testing. Please submit any feedback by sending an email to
-     * friends@segment.com. • In order to successfully call this endpoint, the specified Workspace
-     * needs to have the Audience feature enabled. Please reach out to your customer success manager
-     * for more information.
+     * for an Audience is limited to Linked Audiences (audienceType &#x3D; LINKED). Note that if a
+     * Linked Audience remains disabled for 90 days Segment will delete the associated schedule and
+     * a new schedule will need to be created. • This endpoint is in **Alpha** testing. Please
+     * submit any feedback by sending an email to friends@segment.com. • In order to successfully
+     * call this endpoint, the specified Workspace needs to have the Audience feature enabled.
+     * Please reach out to your customer success manager for more information.
      *
      * @param spaceId (required)
      * @param id (required)
