c43575833bc0fd7016d04e399be49dc933ed597f
APIs and Common Services team <team-apics@twilio.com>
2025-12-04 17:31:37 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@03b94cd1.

--- src/main/java/com/segment/publicapi/Configuration.java
+++ src/main/java/com/segment/publicapi/Configuration.java
@@ -12,7 +12,7 @@
 package com.segment.publicapi;
 
 public class Configuration {
-    public static final String VERSION = "62.1.0";
+    public static final String VERSION = "62.0.4";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
